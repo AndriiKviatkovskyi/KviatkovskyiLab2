ee67e53ee513500e04b1fb4586b76aaa62444e10
AndriiKviatkovskyi <133859288+AndriiKviatkovskyi@users.noreply.github.com>
2024-03-15 09:47:52 +0200

    Add project files.

---
+++ KviatkovskyiLab2/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KviatkovskyiLab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        PersonViewModel _person = new PersonViewModel();
+
+        public ICommand ProceedCommand { get; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ProceedCommand = new RelayCommand(Proceed, CanProceed);
+        }
+
+        private bool CanProceed(object parameter)
+        {
+
+            return !string.IsNullOrWhiteSpace(EnterFirstName.Text) &&
+                   !string.IsNullOrWhiteSpace(EnterLastName.Text) &&
+                   !string.IsNullOrWhiteSpace(EnterEmail.Text) &&
+                   PickedBirthday.SelectedDate.HasValue;
+        }
+
+        private async void Proceed(object parameter)
+        {
+            await ProcessAsync();
+        }
+
+       
+
+        private async Task ProcessAsync()
+        {
+            try
+            {
+                NameBlock.Text = "";
+                SurnameBlock.Text = "";
+                EmailBlock.Text = "";
+                BirthdateBlock.Text = "";
+                AgeBlock.Text = "";
+                IsAdultBlock.Text = "";
+                ZodiacWesternBlock.Text = "";
+                ZodiacChineseBlock.Text = "";
+                BirthdayBlock.Text = "";
+                foreach (UIElement element in MainGrid.Children)
+                {
+                    element.IsEnabled = false;
+                }
+                MainWindowName.IsEnabled = false;
+                string tempName = EnterFirstName.Text;
+                string tempSurname = EnterLastName.Text;
+                string tempEmail = EnterEmail.Text;
+                DateTime tempDate = DateTime.Now;
+                if (PickedBirthday.SelectedDate.HasValue)
+                {
+                    tempDate = PickedBirthday.SelectedDate.Value;
+                }
+                await Task.Run(() =>
+                {
+                    _person.Person = new PersonModel(tempName, tempSurname, tempEmail, tempDate);           
+                });
+                int age = _person.Age;
+                if (age < 0 || age > 135)
+                {
+                    MessageBox.Show("Your age must be between 0 and 135", "INVALID AGE");
+                }
+                else
+                {
+                    NameBlock.Text = _person.FirstName;
+                    SurnameBlock.Text = _person.LastName;
+                    EmailBlock.Text = _person.Email;
+                    BirthdateBlock.Text = _person.Birthdate.ToShortDateString();
+                    AgeBlock.Text = age + "";
+                    IsAdultBlock.Text = _person.IsAdult ? "Adult" : "Child";
+                    ZodiacWesternBlock.Text = _person.ZodiacWestern;
+                    ZodiacChineseBlock.Text = _person.ZodiacChinese;
+                    if (_person.IsBirthday) BirthdayBlock.Text = "HAPPY BIRTHDAY!";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+            finally
+            {
+                foreach (UIElement element in MainGrid.Children)
+                {
+                    element.IsEnabled = true;
+                }
+                MainWindowName.IsEnabled = true;
+            }
+        }
+
+      
+    }
+}
---
+++ KviatkovskyiLab2/PersonViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KviatkovskyiLab2
+{
+    internal class PersonViewModel
+    {
+
+        private PersonModel _person;
+
+        public PersonViewModel()
+        {
+            _person = new PersonModel("", "");
+        }
+
+        public string FirstName
+        {
+            get { return _person.FirstName; }
+            set {  _person.FirstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _person.LastName; }
+            set { _person.LastName = value; }
+        }
+
+        public string Email
+        {
+            get { return _person.Email ?? ""; }
+            set { _person.Email = value; }
+        }
+
+        public DateTime Birthdate
+        {
+            get { return _person.Birthdate ?? DateTime.Today; }
+            set { _person.Birthdate = value; }
+        }
+
+        public int Age
+        {
+            get { return _person.Age ?? -1; }
+        }
+
+        public bool IsAdult
+        {
+            get { return _person.IsAdult ?? false; }
+        }
+
+        public string ZodiacWestern
+        {
+            get { return _person.ZodiacWestern ?? string.Empty; }
+        }
+
+        public string ZodiacChinese
+        {
+            get { return _person.ZodiacChinese ?? string.Empty; }
+        }
+
+        public bool IsBirthday
+        {
+            get { return _person.IsBirthday ?? false; }
+        }
+
+        public PersonModel Person
+        {
+            get { return _person ?? new PersonModel("", "");}
+            set { _person = value; }
+        }
+
+    }
+}
